de462c51e301a158af84e539083645d5a909d833
Your Name <your_email@example.com>
2025-09-10 16:49:05 +0530

    000ZZ

--- 1.sql
+++ 1.sql
@@ -12,6 +12,43 @@ SELECT name
 FROM sys.tables;
 
 
-// query using secExpense create procedure that create  table employ  with data inserted based in argumet to procudure 2 arune anme and number
+
 --@block 
-    CREATE OR ALTER PROCEDURE 
+-- query to check in database  schemas 
+
+USE secExpense;
+GO
+    
+SELECT * FROM sys.schemas;
+
+
+
+--@block
+-- query using secExpense create procedure that create  table employ  with data inserted based in argumet to procudure 2 arune anme and number
+
+USE secExpense;
+GO
+
+--@block 
+CREATE OR ALTER PROCEDURE dbo.CreateEmployWithData
+    @EmpName NVARCHAR(100),
+    @EmpNumber INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'employ')
+    BEGIN
+        CREATE TABLE dbo.employ (
+            ID INT IDENTITY(1,1) PRIMARY KEY,
+            EmpName NVARCHAR(100) NOT NULL,
+            EmpNumber INT NOT NULL
+        );
+    END
+
+    INSERT INTO dbo.employ (EmpName, EmpNumber)
+    VALUES (@EmpName, @EmpNumber);
+END;
+GO
+
+exec dbo.CreateEmployWithData @EmpName = 'John Doe', @EmpNumber = 1234567890;
