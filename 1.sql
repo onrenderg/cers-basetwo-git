a81525aeb1eb3c6c10219d3f8f51f6241c0984f7
Your Name <your_email@example.com>
2025-09-10 16:43:52 +0530

    000Z

---
+++ 1.sql
@@ -0,0 +1,17 @@
+--@block
+
+SELECT name 
+FROM sys.databases;
+
+
+
+--@block
+USE secExpense;
+GO
+SELECT name 
+FROM sys.tables;
+
+
+// query using secExpense create procedure that create  table employ  with data inserted based in argumet to procudure 2 arune anme and number
+--@block 
+    CREATE OR ALTER PROCEDURE 
