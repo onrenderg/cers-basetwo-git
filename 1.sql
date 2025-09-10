1f8aece4902387e0fd4b91d284217c58e1efafbe
Your Name <your_email@example.com>
2025-09-10 16:58:59 +0530

    0002

--- 1.sql
+++ 1.sql
@@ -52,3 +52,53 @@ END;
 GO
 
 exec dbo.CreateEmployWithData @EmpName = 'John Doe', @EmpNumber = 1234567890;
+
+
+
+--@block 
+CREATE OR ALTER PROCEDURE dbo.CreateEmployWithData
+    @EmpName NVARCHAR(100),
+    @EmpNumber INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'employ')
+    BEGIN
+        CREATE TABLE dbo.employ (
+            ID INT IDENTITY(1,1) PRIMARY KEY,
+            EmpName NVARCHAR(100) NOT NULL,
+            EmpNumber INT NOT NULL
+        );
+    END
+
+    INSERT INTO dbo.employ (EmpName, EmpNumber)
+    VALUES (@EmpName, @EmpNumber);
+END;
+
+
+
+exec dbo.CreateEmployWithData @EmpName = 'John Doe', @EmpNumber = 1234567890;
+
+
+
+
+
+exec dbo.CreateEmployWithData @EmpName = 'John Doe', @EmpNumber = 1234567890;
+
+use secExpense
+
+select name 
+from sys.tables
+
+select * from sys.databases;
+
+
+
+select * from sys.tables
+
+USE secExpense;
+
+-- look at the table
+SELECT * FROM sys.tables WHERE name = 'employ'
+
